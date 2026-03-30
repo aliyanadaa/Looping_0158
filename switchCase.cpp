06415b48655b93ddf388578bdddcb4cc3e445c3b
aliyanadaa <aliyanadaq@gmail.com>
2026-03-30 17:41:35 +0700

    implementasi perulangan for

--- switchCase.cpp
+++ switchCase.cpp
@@ -2,3 +2,13 @@
 #include <ctime>
 using namespace std;
 
+int main (){
+    int x;
+
+    cout<<"PERUULANGAN FOR"<<endl
+    for(int i=0; i<=4; i++){
+        cout<<"Teknologi Informasi UMY" <<endl;
+    }
+    cout<<endl;
+    sprand(time(0));
+}
