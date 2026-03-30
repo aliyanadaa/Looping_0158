be2c3cf3256dcffda3b33d35eced3cf3c1be5844
aliyanadaa <aliyanadaq@gmail.com>
2026-03-30 18:47:02 +0700

    implementasi WHILE

--- switchCase.cpp
+++ switchCase.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+
     int x;
 
     cout<<"PERULANGAN FOR"<<endl;
@@ -11,6 +12,13 @@ int main(){
     }
     cout<<endl;
     srand(time(0));
+
+cout<<"PERULANGAN WHILE"<<endl;
+x = 1 + rand()%10;
+
+while(x<=5){
+    cout<<"Bilangan acak ="<<x <<endl;
+    x = rand()%10;
 }
 
-
+}
